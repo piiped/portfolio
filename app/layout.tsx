026ef11d9e58792b15a219b559c7224b1ce62263
piiped <aomkadang@gmail.com>
2024-06-06 10:51:37 +0700

    added icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Papitchaya Kadang",
   description: "Papitchaya's portfolio",
+  icons: {
+    icon: '/icon.png', // /public path
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="icon" href="/icon.png" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
   );
